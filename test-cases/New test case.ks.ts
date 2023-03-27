3a10ecce3f321fb02f472d97643b4bd6525db1d3
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-03-27 23:50:39 +0700

    Katalon: Update test case New test case in test-cases

--- test-cases/New test case.ks.ts
+++ test-cases/New test case.ks.ts
@@ -1,5 +1,37 @@
 import katalon from "katalon";
 
 export default katalon.testCase("New Test Case", async ({ local, web }) => {
-  await web.openBrowser("https://demoaut.katalon.com");
+  await web.openBrowser("https://katalon-demo-cura.herokuapp.com");
+  await web.click({
+    type: "Web",
+    id: "d520938e-b2c7-442b-9bf7-f25ecf577a21",
+    attributes: {
+      id: "btn-make-appointment",
+      href: "./profile.php#login",
+      class: "btn btn-dark btn-lg",
+    },
+    childIndex: 4,
+    hashes: { "md5.v1": "3caec008087ccb7a1cb3fc698fbf83c2" },
+    name: "a - Make Appointment",
+    selectors: [
+      {
+        id: "f85df66b-82ba-4aea-b0be-f83ca14e5e93",
+        type: "CSS",
+        value: "#btn-make-appointment",
+        isDefault: true,
+      },
+      {
+        id: "51f87666-9b12-419e-9414-532afc4b1329",
+        type: "Attribute",
+        value: null,
+        isDefault: true,
+      },
+    ],
+    selectorType: "CSS",
+    tag: "a",
+    text: "Make Appointment",
+    pageUrl: "https://katalon-demo-cura.herokuapp.com/",
+    parentIframe: null,
+    shadowRoot: null,
+  });
 });
