8fb369591206282d6cf352e0b956ff8bb5a6c3f4
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-05 16:44:54 +0700

    zncncz

---
+++ test-suites/abc.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>abc</name>
+    <testSuiteGuid>0a19a942-ce78-45d6-9cfa-9f90fcee1dc1</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>Test Cases/Find user by ID</testCaseId>
+        <guid>944455d8-e9c4-43e4-9d8f-067b8064ddd8</guid>
+    </testCaseLink>
+</TestSuiteEntity>
