2cef057b328dbb1519c582c511e6a28d915b1e93
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-05 14:36:54 +0700

    kkk

---
+++ test-suites/k.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>k</name>
+    <testSuiteGuid>15b949b0-7078-4d8b-ad01-138d1a33d14b</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>Test Cases/Create a new user</testCaseId>
+        <guid>89d7fa28-0936-4112-bd1f-e7d6f45bc396</guid>
+    </testCaseLink>
+</TestSuiteEntity>
