b988d29da93b188d96d9ef83b61c8258f2ef81ec
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-05 16:43:53 +0700

    nanananana

---
+++ test-suites/ka.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>ka</name>
+    <testSuiteGuid>812fd328-9b4f-426d-9a01-5747ef52fb0c</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/hiep-vu/New test case</testCaseId>
+        <guid>3e6df6f9-5d02-4d56-8174-bc5945ebf520</guid>
+    </testCaseLink>
+</TestSuiteEntity>
