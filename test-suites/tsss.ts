87b321376f2721c46d83b4f81dc394ae356e2fa8
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-07 16:05:54 +0700

    abc

---
+++ test-suites/tsss.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>tsss</name>
+    <testSuiteGuid>e7683490-d9c5-47fa-b178-ac6c1efbb387</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>Test Cases/Create a new user</testCaseId>
+        <guid>58ff2027-e28f-4c90-8399-ab27d4d51365</guid>
+    </testCaseLink>
+</TestSuiteEntity>
