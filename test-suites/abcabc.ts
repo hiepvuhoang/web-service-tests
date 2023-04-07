f2da1400725410ffae541d14a2897b92fd97493f
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-07 15:13:53 +0700

    cbz

---
+++ test-suites/abcabc.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>abcabc</name>
+    <testSuiteGuid>7126ab46-451a-4005-9b17-87a0e97c7398</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>Test Cases/Create a new user</testCaseId>
+        <guid>c6e12375-53b2-46b2-aa8c-a46ff65d0042</guid>
+    </testCaseLink>
+</TestSuiteEntity>
