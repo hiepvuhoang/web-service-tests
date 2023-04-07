254a75bcd41e0175071e23636703d72a66f5a6ad
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-07 14:52:22 +0700

    a

---
+++ test-suites/m.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>m</name>
+    <testSuiteGuid>57167032-bd03-4e7f-b618-b50004ef6e77</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>Test Cases/Create a new user</testCaseId>
+        <guid>28ee8265-f1eb-4475-becc-cbb022077f68</guid>
+    </testCaseLink>
+</TestSuiteEntity>
