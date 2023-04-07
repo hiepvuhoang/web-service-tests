8cbd5cf1c8dfc53c703aa59c690dce5da3ba038f
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-07 15:58:16 +0700

    im

---
+++ test-suites/tss.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>tss</name>
+    <testSuiteGuid>5021d50d-850c-4364-9741-e9ca72818595</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>Test Cases/Find user by ID</testCaseId>
+        <guid>056945cc-f93b-432c-8526-9c22273a0ae9</guid>
+    </testCaseLink>
+</TestSuiteEntity>
