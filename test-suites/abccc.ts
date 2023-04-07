cd877997a421390ea8720f45bedf27265da1e805
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-07 16:56:36 +0700

    a

---
+++ test-suites/abccc.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>abccc</name>
+    <testSuiteGuid>3cca2d34-15f8-480c-b25a-1e50ec9d4cc5</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>Test Cases/Create a new user</testCaseId>
+        <guid>5bfa6923-9cd2-42bd-affc-49412a0d29f5</guid>
+    </testCaseLink>
+</TestSuiteEntity>
