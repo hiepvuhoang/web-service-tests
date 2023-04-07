1668a857fb02e69f48a73f4abfff649ef47e223c
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-07 17:02:14 +0700

    a

---
+++ test-suites/abczz.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>abczz</name>
+    <testSuiteGuid>3a897cd3-43ad-4466-a5ca-6cdb27a35853</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>Test Cases/Create a new user</testCaseId>
+        <guid>7f172b39-841b-484e-84e5-5f6aa12b2c47</guid>
+    </testCaseLink>
+</TestSuiteEntity>
