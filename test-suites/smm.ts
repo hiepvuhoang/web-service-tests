192a7143ae0e88380743b3d05f88a00aaedf8510
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-07 17:06:18 +0700

    x

---
+++ test-suites/smm.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>smm</name>
+    <testSuiteGuid>1aba2911-5126-4eb2-bbaa-7645a71329db</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>Test Cases/Find user by ID</testCaseId>
+        <guid>b1144f2f-5695-4be8-854b-f0381a9fd1a5</guid>
+    </testCaseLink>
+</TestSuiteEntity>
