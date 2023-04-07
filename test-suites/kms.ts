afda010333e050d2c9b1e61d5adfdf0c99649f23
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-07 15:08:57 +0700

    abcbzbz

---
+++ test-suites/kms.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>kms</name>
+    <testSuiteGuid>34b2ceb5-5deb-4066-a71b-d666d4830e8c</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>Test Cases/Create a new user</testCaseId>
+        <guid>186b476b-ee62-46dd-acf8-db57b2490d4c</guid>
+    </testCaseLink>
+</TestSuiteEntity>
