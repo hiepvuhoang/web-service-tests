e16498bfc377a6ade6f6ecfb0ffca3a7b58e92d9
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-07 16:39:37 +0700

    z

---
+++ test-suites/zz.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>zz</name>
+    <testSuiteGuid>6f909544-29fc-4203-8fa2-025fc6da7851</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>Test Cases/Create a new user</testCaseId>
+        <guid>dc9c92f7-899b-4ec8-b981-291e2f956596</guid>
+    </testCaseLink>
+</TestSuiteEntity>
