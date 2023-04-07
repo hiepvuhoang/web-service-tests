fd1e9ae47440817ecbb0233e25dd41a59548eda1
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-07 14:41:34 +0700

    mn

---
+++ test-suites/kkkk.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>kkkk</name>
+    <testSuiteGuid>a02ead3c-52b0-4a3a-81be-9292a342d7e5</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>Test Cases/Find user by ID</testCaseId>
+        <guid>dc8cb0b6-f4ad-4a9b-990d-c7ecc640be82</guid>
+    </testCaseLink>
+</TestSuiteEntity>
