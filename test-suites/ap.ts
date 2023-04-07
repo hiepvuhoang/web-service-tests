a51e675feaf74c5fdae7b33118e92954882a0ed0
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-07 17:08:44 +0700

    z

---
+++ test-suites/ap.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>ap</name>
+    <testSuiteGuid>7ed12aea-8021-4038-847c-7ae06c4a4ef1</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>Test Cases/Create a new user</testCaseId>
+        <guid>6e1001ed-76ea-4f59-aa67-6ea0fe8ed0b7</guid>
+    </testCaseLink>
+</TestSuiteEntity>
