bfc6254feeebd14000846e79d99d969b9459cb1f
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-07 15:03:53 +0700

    dd

---
+++ test-suites/bbbbb.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>bbbbb</name>
+    <testSuiteGuid>2735f87d-ae04-4b06-84c7-fb4caf48cb9b</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>Test Cases/Create a new user</testCaseId>
+        <guid>c57df83b-81ae-49c0-989d-ae6ad88189a7</guid>
+    </testCaseLink>
+</TestSuiteEntity>
