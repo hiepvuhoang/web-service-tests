6b021b3d54bc7a3afed33b456949e83e91c034e9
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-07 17:04:07 +0700

    z

---
+++ test-suites/abp.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>abp</name>
+    <testSuiteGuid>72b8afd4-25d9-4104-a7a2-42592169c1a8</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/hiep-vu/hiep 1/New test case (1)</testCaseId>
+        <guid>f2f20f4c-4f38-4c1d-9ac1-b022eed99430</guid>
+    </testCaseLink>
+</TestSuiteEntity>
