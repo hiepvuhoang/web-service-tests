c942cb532b75600536a5653f8dc2c54915cbbc2b
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-10 11:09:32 +0700

    Mbappe

---
+++ test-suites/Kylian Mbappe.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Kylian Mbappe</name>
+    <testSuiteGuid>10985258-5fcf-49a0-912e-ef647e2c21b9</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>Test Cases/Find user by ID</testCaseId>
+        <guid>08dc9280-6686-41dc-a1dd-36310f254a36</guid>
+    </testCaseLink>
+</TestSuiteEntity>
