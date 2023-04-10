00a7e626d1242b4d89d6b2d36c73cf478740303c
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-10 10:53:11 +0700

    a

---
+++ test-suites/ancc.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>ancc</name>
+    <testSuiteGuid>594b6dc2-0281-409f-864b-a768b2864cf9</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>Test Cases/gen5tc</testCaseId>
+        <guid>5aab240f-f8e4-491b-9cc2-a8388f1ea3ef</guid>
+    </testCaseLink>
+</TestSuiteEntity>
