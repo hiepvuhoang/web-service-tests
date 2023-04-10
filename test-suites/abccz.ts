1dbc74f69e0a2c77890adc1706095fc9a265ec94
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-10 10:56:37 +0700

    m

---
+++ test-suites/abccz.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>abccz</name>
+    <testSuiteGuid>485a542d-e861-4f37-8f3c-357201665460</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>Test Cases/Find user by ID</testCaseId>
+        <guid>56b14c78-0715-4624-bb06-5c6792af78f3</guid>
+    </testCaseLink>
+</TestSuiteEntity>
