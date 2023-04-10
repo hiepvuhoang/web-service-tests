8b7dde954e2cbe7c131ec720a1d4299aa6d38662
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-10 10:50:29 +0700

    n

---
+++ test-suites/mabccc.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>mabccc</name>
+    <testSuiteGuid>43168fad-0b6b-462a-b9be-14625687721d</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>Test Cases/Find user by ID</testCaseId>
+        <guid>555907c6-1df5-43e7-a419-fc77b203855d</guid>
+    </testCaseLink>
+</TestSuiteEntity>
