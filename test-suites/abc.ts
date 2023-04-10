a1a7276e0b132b6623125d4998a6fcbc4217d1ff
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-10 16:50:51 +0700

    m

--- test-suites/abc.ts
+++ test-suites/abc.ts
@@ -1,9 +1,9 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
     <name>abc</name>
-    <testSuiteGuid>0a19a942-ce78-45d6-9cfa-9f90fcee1dc1</testSuiteGuid>
+    <testSuiteGuid>7880a46b-72f5-4afe-94f3-8307ca361dce</testSuiteGuid>
     <testCaseLink>
-        <testCaseId>Test Cases/Find user by ID</testCaseId>
-        <guid>944455d8-e9c4-43e4-9d8f-067b8064ddd8</guid>
+        <testCaseId>Test Cases/Create a new user</testCaseId>
+        <guid>338e2028-7872-489f-a88b-1d23d1a03db5</guid>
     </testCaseLink>
 </TestSuiteEntity>
