b0e27f2746e37dc4a358de9e944c95f33a994199
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-10 11:18:45 +0700

    V

---
+++ test-suites/Valverde.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Valverde</name>
+    <testSuiteGuid>ea221457-5182-4336-8736-f35a9a776a9a</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>Test Cases/Find user by ID</testCaseId>
+        <guid>6565454f-ac0f-4914-ac9e-8537ea239739</guid>
+    </testCaseLink>
+</TestSuiteEntity>
