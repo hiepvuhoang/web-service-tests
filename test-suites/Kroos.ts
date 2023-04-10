d561378e548f276059fc63c91a117b8f2ca92f72
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-10 17:14:31 +0700

    Kroos

---
+++ test-suites/Kroos.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Kroos</name>
+    <testSuiteGuid>9188fd64-eae3-4c34-b848-769ef51f6445</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>Test Cases/gen5tc</testCaseId>
+        <guid>aa82b45f-9f15-40d4-ac9c-0fb2fb3a105e</guid>
+    </testCaseLink>
+</TestSuiteEntity>
