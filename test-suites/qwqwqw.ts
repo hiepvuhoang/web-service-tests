8debd0f20ca1c123e91553c3bb7364d239071579
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-10 17:19:44 +0700

    qwqwqw

---
+++ test-suites/qwqwqw.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>qwqwqw</name>
+    <testSuiteGuid>daa0a5cb-7d61-4f3e-b293-f451dbad70cf</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>Test Cases/Find user by ID</testCaseId>
+        <guid>c003d72e-4f14-4a09-9f9a-ab8b3c386de0</guid>
+    </testCaseLink>
+</TestSuiteEntity>
