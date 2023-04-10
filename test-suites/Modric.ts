bba400539c7ffba1e0a9186c1a5dee27ff27113e
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-10 11:25:52 +0700

    modridc

---
+++ test-suites/Modric.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Modric</name>
+    <testSuiteGuid>c7a64d37-3494-447d-a780-7c103f95a86e</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>Test Cases/gen5tc</testCaseId>
+        <guid>2aa745ef-ccbb-4eec-bef0-1a5a58c6c7a5</guid>
+    </testCaseLink>
+</TestSuiteEntity>
