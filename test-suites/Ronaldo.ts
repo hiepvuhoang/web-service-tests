26bd14459642fca9c265178aad9228238e1895b6
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-10 11:00:09 +0700

    m

---
+++ test-suites/Ronaldo.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Ronaldo</name>
+    <testSuiteGuid>e5e17510-11ac-49e2-ab46-24832b59b81a</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>Test Cases/Find user by ID</testCaseId>
+        <guid>66c046f8-4a56-4451-a1e9-155979e887d8</guid>
+    </testCaseLink>
+</TestSuiteEntity>
