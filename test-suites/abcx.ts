c18c03d87a7d117261b6daf423424b9a3f7dcc2d
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-10 09:57:50 +0700

    cb

---
+++ test-suites/abcx.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>abcx</name>
+    <testSuiteGuid>98ba52db-bfb2-4335-8b12-4a26ac064c7e</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/hiep-vu/New test case</testCaseId>
+        <guid>80e9d180-fd05-489e-af86-8ebfaaaf3ce0</guid>
+    </testCaseLink>
+</TestSuiteEntity>
