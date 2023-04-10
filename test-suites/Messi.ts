03c63e6f3d7ba34b9d239e5ffd0f6fbff580b741
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-10 11:07:43 +0700

    Messi

---
+++ test-suites/Messi.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Messi</name>
+    <testSuiteGuid>14322c66-3525-4dc3-ada2-718c376b6377</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>Test Cases/Find user by ID</testCaseId>
+        <guid>1b684397-1896-457f-9e0b-b4f89ec41be1</guid>
+    </testCaseLink>
+</TestSuiteEntity>
