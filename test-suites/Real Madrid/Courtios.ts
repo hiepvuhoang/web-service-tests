ed90c4bb72c5aa2f0f1a63ce5bf6cfc44f050141
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-10 17:21:39 +0700

    abc

---
+++ test-suites/Real Madrid/Courtios.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Courtios</name>
+    <testSuiteGuid>a33b55a7-cf84-4855-901f-fced7cfb4642</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>Test Cases/gen5tc</testCaseId>
+        <guid>edf88be3-85bb-4a51-a278-61f7981e76e3</guid>
+    </testCaseLink>
+</TestSuiteEntity>
