dfe9da1443cf536652f2f811610bdc0d38c1c1fa
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-10 09:22:08 +0700

    n

---
+++ test-suites/abczxc.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>abczxc</name>
+    <testSuiteGuid>7305e0bf-53c1-4952-901c-76624d0e5876</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/hiep-vu/hiep 1/New test case (1)</testCaseId>
+        <guid>3e6b640f-5813-4679-8a59-b3d562d229ea</guid>
+    </testCaseLink>
+</TestSuiteEntity>
