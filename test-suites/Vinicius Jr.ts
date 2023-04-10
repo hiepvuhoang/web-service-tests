1e0ce79c88223b73fbebf3bacf61d0cbcd6de457
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-10 11:23:43 +0700

    Vj

---
+++ test-suites/Vinicius Jr.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Vinicius Jr</name>
+    <testSuiteGuid>e3ceefe2-58f5-4e8c-a6c5-4f8f7cd289d8</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>Test Cases/Create a new user</testCaseId>
+        <guid>ff59294c-4759-48bf-aceb-cf5bb6f55fc7</guid>
+    </testCaseLink>
+</TestSuiteEntity>
