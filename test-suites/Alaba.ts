7cde7f9de821068a26fddc7f5d1039712daabc67
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-10 17:22:17 +0700

    alaba

---
+++ test-suites/Alaba.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Alaba</name>
+    <testSuiteGuid>dd6ea553-0fcd-42ac-a59d-395717458300</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>Test Cases/gen5tc</testCaseId>
+        <guid>49a323a5-f66a-49b3-ae6a-091dbd2266c2</guid>
+    </testCaseLink>
+</TestSuiteEntity>
