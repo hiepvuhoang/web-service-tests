bb870922d45f172e8eff65fcd9bb14c3155894fb
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-10 10:00:42 +0700

    b

---
+++ test-suites/mn.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>mn</name>
+    <testSuiteGuid>ec565d3b-768d-4d96-9c1c-31562c791e9d</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/hiep-vu/New test case</testCaseId>
+        <guid>ad7f0125-96b6-468e-91c9-dd7323b10b02</guid>
+    </testCaseLink>
+</TestSuiteEntity>
