b9ecbed6d95928eb642df2aaa318c92a9436ec05
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-04-10 09:51:40 +0700

    m

---
+++ test-suites/po.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>po</name>
+    <testSuiteGuid>a8fd7fba-9d70-4200-ace0-af26a14b0d49</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>Test Cases/gen5tc</testCaseId>
+        <guid>4f0b1342-636c-4885-a6e9-4ff48713b20f</guid>
+    </testCaseLink>
+</TestSuiteEntity>
