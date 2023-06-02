de0e0cdf5ea14026e5e0d9e4fa31cf737dea9101
hiepvuhoang <79436125+hiepvuhoang@users.noreply.github.com>
2023-06-02 15:00:23 +0700

    sax

---
+++ test-suites/Real Madrid/a.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>a</name>
+    <testSuiteGuid>d9fb42a1-8d99-45c7-a8e0-ba551a6e0e37</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/hiep-vu/hiep 2/New test case (1)</testCaseId>
+        <guid>33cf830c-4cc8-4244-9c60-645a4431cbf1</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/hiep-vu/hiep 1/New test case (1)</testCaseId>
+        <guid>746cd7b5-387e-4ac3-9d5f-273a34d0a18b</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/hiep-vu/hiep 1/New test case anabba</testCaseId>
+        <guid>b9ac56e3-eaa3-4d0e-999c-00b07c76d8aa</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/hiep-vu/New test case</testCaseId>
+        <guid>24295948-4e1c-4eea-8b30-f6b5eb5a053a</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/dump                         .folder/X0l8GDrdQY3xtLu2wa7ggETHyYMPssfKhShyx0o6VDYpeu1o8YBybxVO5q3IxgSuHbKROy1XnnN4YgfnC9FsVGAM1GAk2ePaxOcWwzmfiF7dDRjcFIG5p81P7qAamFfxkntIAiXbOm3xVzdHPzFnSSMM5KLltGBODx4XnMbwkDw1H834HZnZEJ7kWrrY4gSryZ8TUYQtkoKGkMrGMEoic7ZjoYQtdC6OR84MEvywNNdfiQapHLN8vIL5WvmXcfD/Meow</testCaseId>
+        <guid>f4b7bdcd-83f0-4419-8735-ac0e6a8408e8</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/dump                         .folder/dump</testCaseId>
+        <guid>35a57f07-6c76-40a1-acf2-0bdd7868ad96</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Create a new user</testCaseId>
+        <guid>cdc8144c-65da-4ff9-80bf-9499799fee5b</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/New test case</testCaseId>
+        <guid>bfa39bf7-ad85-411d-80fe-917957ca270b</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/New test case (1)</testCaseId>
+        <guid>2f51a9c2-477e-44b0-bad0-4cf12e55802c</guid>
+    </testCaseLink>
+</TestSuiteEntity>
